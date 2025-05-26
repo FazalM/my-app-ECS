b9d92637f9dd22dec972ca09b57115f007d9a0d9
devops4sure <devops4sure@gmail.com>
2025-05-26 21:21:26 +0100

    run the full CI process

--- CI-pipeline/main.tf
+++ CI-pipeline/main.tf
@@ -11,6 +11,7 @@ terraform {
 resource "aws_ecr_repository" "my_ecr_app_repo" {
   name                 = "my-ecr-app-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
